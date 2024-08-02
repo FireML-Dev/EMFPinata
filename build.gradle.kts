71707ca3b99ff329fab733dc8d714ab58bc07ae4
FireML <firemlyt@gmail.com>
2024-08-02 12:48:12 +0100

    1.0.3 Release

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 }
 
 group = "uk.firedev"
-version = "1.0.3-SNAPSHOT"
+version = "1.0.3"
 description = "A Piñata addon for the EvenMoreFish plugin."
 java.sourceCompatibility = JavaVersion.VERSION_21
 
