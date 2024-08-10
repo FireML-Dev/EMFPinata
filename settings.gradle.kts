fc8d9b04a0aed4e1f113b306ad7f669c4be082c8
FireML <firemlyt@gmail.com>
2024-08-10 13:31:57 +0100

    Paper 1.21.1
    Not worthy of a new release as everything is compatible

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "EMFPinata"
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            library("paper-api", "io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+            library("paper-api", "io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
             library("evenmorefish", "com.oheers:EvenMoreFish:1.7.2")
             library("commandapi", "dev.jorel:commandapi-bukkit-shade-mojang-mapped:9.5.1")
             library("bstats", "org.bstats:bstats-bukkit:3.0.2")
